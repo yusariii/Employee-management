f7653baa9995835d7806cf4f8242af82fb966167
Yusariii <khailenguyen2004@gmail.com>
2026-03-16 11:27:09 +0700

    Auto clean otp

--- src/main/java/com/khai/em/EmployeeApiApplication.java
+++ src/main/java/com/khai/em/EmployeeApiApplication.java
@@ -1,7 +1,11 @@
 package com.khai.em;
+
+import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 @SpringBootApplication
+@EnableScheduling
 public class EmployeeApiApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(EmployeeApiApplication.class, args);
